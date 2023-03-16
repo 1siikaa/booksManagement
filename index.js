7b04a0505e528b6784a15d30ed1c1c0ae29529da
1siikaa <srishtidwivedi672@gmail.com>
2023-03-16 21:03:43 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const route = require('../route/route');
+const route = require('./src/route/route');
 const app = express()
+const cors = require('cors')
 
 
 const multer= require("multer");
@@ -10,7 +11,7 @@ app.use( multer().any())
 app.use(express.json())
 
 
-mongoose.connect("mongodb+srv://Vashika:Vanshikaa08@cluster0.on6mcgr.mongodb.net/test")
+mongoose.connect("mongodb+srv://1siikaa07:1siikaa07@cluster0.lhrj8le.mongodb.net/booksManagement?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB is connected"))
 .catch((error) => console.log(error))
 
@@ -25,5 +26,5 @@ app.use(function(req, res, next) {
 app.use('/', route);
 
 
-app.listen(3000, function () {
-console.log('Express app running on port ' + 3000)})
+app.listen(3001, function () {
+console.log('Express app running on port ' + 3001)})
