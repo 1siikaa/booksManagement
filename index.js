10069c245a7c45a1758406037177035932471902
1siikaa <srishtidwivedi672@gmail.com>
2023-03-16 20:03:13 +0530

    done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 
 
 const multer= require("multer");
-
+app.use(cors())
 app.use( multer().any())
 app.use(express.json())
 
@@ -14,7 +14,13 @@ mongoose.connect("mongodb+srv://Vashika:Vanshikaa08@cluster0.on6mcgr.mongodb.net
 .then(() => console.log("MongoDB is connected"))
 .catch((error) => console.log(error))
 
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://main--adorable-croissant-6c39f3.netlify.app/');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+    });
 
 app.use('/', route);
 
