53d9a5ce6af2eaf2ff6b54d12527dd0f88cae54b
1siikaa <srishtidwivedi672@gmail.com>
2023-03-16 21:43:52 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const route = require('./src/route/route');
+const route = require('../src/route/route');
 const app = express()
 const cors = require('cors')
 
